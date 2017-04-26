b07db0e43fa95768abcde06cefeb0c278c51daef
cdadar <gunbanmi@gmail.com>
2017-04-26 14:08:19 +0800

    modofied custom.el

--- custom.el
+++ custom.el
@@ -3,18 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-bright)))
  '(custom-safe-themes
    (quote
-    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+    ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
  '(delete-selection-mode t)
- '(erc-autojoin-channels-alist (quote (("freenode.net" "#opensuse-cn" "#lisp"))))
  '(org-agenda-files
    (quote
     ("~/data/org-model/projects.org" "~/data/org-model/plan.org")))
  '(package-selected-packages
    (quote
-    (ox-latex xref-js2 wgrep-ag ag yasnippet yari yaml-mode yagist whole-line-or-region whitespace-cleanup-mode wgrep vc-darcs uptimes unfill undo-tree tidy textile-mode tagedit switch-window sql-indent smex smarty-mode slime-company skewer-less session scss-mode scratch sass-mode ruby-hash-syntax rspec-mode robe rinari regex-tool redshank rainbow-mode rainbow-delimiters projectile-rails project-local-variables pip-requirements php-mode paredit-everywhere page-break-lines origami org-pomodoro org-fstree org-cliplink nlinum multiple-cursors move-dup mmm-mode markdown-mode magit-gh-pulls lua-mode lively less-css-mode ledger-mode keyfreq json-mode js-comint ivy-historian ipretty intero indent-guide immortal-scratch ibuffer-vc hungry-delete htmlize hl-sexp hippie-expand-slime hindent highlight-symbol highlight-quoted highlight-escape-sequences hayoo guide-key goto-gem gnuplot gitignore-mode github-issues github-clone gitconfig-mode git-timemachine git-messenger git-blamed fullframe flycheck-package flycheck-ledger flycheck-elm flycheck-clojure fill-column-indicator expand-region exec-path-from-shell erlang emmet-mode elm-mode elisp-slime-nav elein dsvn disable-mouse dired-sort dired+ diminish diff-hl default-text-scale darcsum csv-nav csv-mode css-eldoc crontab-mode counsel company-quickhelp company-anaconda command-log-mode color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized coffee-mode cmd-to-echo cljsbuild-mode cl-lib-highlight chinese-fonts-setup cask-mode bundler bug-reference-github browse-kill-ring browse-at-remote avy auto-compile anzu aggressive-indent)))
+    (uptimes regex-tool dsvn htmlize lua-mode gnuplot keyfreq chinese-fonts-setup hungry-delete emmet-mode yasnippet flycheck-ledger ledger-mode origami flycheck-clojure cider elein cljsbuild-mode clojure-mode slime-company hippie-expand-slime slime cask-mode flycheck-package highlight-quoted macrostep cl-lib-highlight aggressive-indent redshank immortal-scratch hl-sexp auto-compile ipretty lively elisp-slime-nav paredit-everywhere paredit sql-indent projectile-rails rinari yaml-mode bundler goto-gem yari robe ruby-compilation inf-ruby rspec-mode ruby-hash-syntax flycheck-elm elm-mode hayoo hindent intero haskell-mode company-anaconda anaconda-mode pip-requirements css-eldoc skewer-less less-css-mode scss-mode sass-mode rainbow-mode tagedit tidy ox-ioslide org-pomodoro org-cliplink org-fstree smarty-mode php-mode skewer-mode js-comint xref-js2 coffee-mode js2-mode json-mode erlang csv-nav csv-mode markdown-mode textile-mode crontab-mode cmd-to-echo alert projectile magit-gh-pulls github-issues github-clone bug-reference-github yagist git-messenger magit git-timemachine gitconfig-mode gitignore-mode git-blamed vc-darcs darcsum browse-at-remote fill-column-indicator whitespace-cleanup-mode guide-key highlight-escape-sequences whole-line-or-region move-dup page-break-lines multiple-cursors avy expand-region browse-kill-ring highlight-symbol undo-tree rainbow-delimiters nlinum indent-guide unfill mmm-mode default-text-scale session switch-window company-quickhelp company counsel ivy-historian ivy smex flycheck ibuffer-vc wgrep-ag ag anzu diff-hl dired-sort dired+ disable-mouse color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized command-log-mode scratch diminish project-local-variables wgrep exec-path-from-shell fullframe)))
  '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
