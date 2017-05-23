3abea73a9a2d21fdf4975f70510f8f35cfde8f45
cdadar <gunbanmi@gmail.com>
2017-05-23 11:45:00 +0800

    modified js indent and emmit setting

--- lisp/init-emmet.el
+++ lisp/init-emmet.el
@@ -1,12 +1,14 @@
-(require-package 'emmet-mode)
-(add-hook 'web-mode-hook 'emmet-mode)
-(add-hook 'html-mode-hook 'emmet-mode)
-(add-hook 'sgml-mode-hook 'emmet-mode)
-(add-hook 'css-mode-hook  'emmet-mode)
+(when (maybe-require-package 'emmet-mode)
+  (add-hook 'web-mode-hook 'emmet-mode)
+  (add-hook 'html-mode-hook 'emmet-mode)
+  (add-hook 'sgml-mode-hook 'emmet-mode)
+  (add-hook 'css-mode-hook  'emmet-mode)
+  (add-hook 'js-mode-hook 'emmet-mode))
 
-(eval-after-load 'emmet-mode '(progn
-                                (setq emmet-move-cursor-between-quotes t)
-                                (setq emmet-expand-jsx-className? t)
-                                (setq emmet-self-closing-tag-style " /")))
+
+(after-load 'emmet-mode
+  (setq emmet-move-cursor-between-quotes t)
+  (setq emmet-expand-jsx-className? t)
+  (setq emmet-self-closing-tag-style " /"))
 
 (provide 'init-emmet)
