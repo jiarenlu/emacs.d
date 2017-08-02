19815ef044ea67c490cb7c55876a13267aa53d25
cdadar <gunbanmi@gmail.com>
2017-08-02 10:52:59 +0800

    unbind C-; in flyspell

--- lisp/init-flyspell.el
+++ lisp/init-flyspell.el
@@ -4,7 +4,8 @@
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
 
 (after-load 'flyspell
-  (add-to-list 'flyspell-prog-text-faces 'nxml-text-face))
+  (add-to-list 'flyspell-prog-text-faces 'nxml-text-face)
+  (define-key flyspell-mode-map (kbd "C-;") nil))
 
 
 (provide 'init-flyspell)
