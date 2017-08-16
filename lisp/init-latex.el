2869ddcde8e1e44b0878acdb701eaa51e379b1fd
cdadar <gunbanmi@gmail.com>
2017-08-16 22:38:00 +0800

    drop cdlatex

--- lisp/init-latex.el
+++ lisp/init-latex.el
@@ -1,12 +1,7 @@
 (require-package 'auctex)
-(require-package 'cdlatex)
 
-;;----------------------------------------------------------------------------
-;; cdlatex
-;;----------------------------------------------------------------------------
 (add-hook 'LaTeX-mode-hook
           (lambda ()
-            (cdlatex-mode t)
             (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
             (setq TeX-command-default "XeLaTeX")
             (setq TeX-save-query  nil )
