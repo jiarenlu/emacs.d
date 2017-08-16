631e040b5596991e7755b18bace400a4d4748b75
cdadar <gunbanmi@gmail.com>
2017-08-16 15:09:37 +0800

    add auctex

--- lisp/init-latex.el
+++ lisp/init-latex.el
@@ -1,8 +1,15 @@
-(add-hook 'LaTeX-mode-hook (lambda()
-                             (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
-                             (setq TeX-command-default "XeLaTeX")
-                             (setq TeX-save-query  nil )
-                             (setq TeX-show-compilation t)
-                             ))
+(require-package 'auctex)
+(require-package 'cdlatex)
+
+;;----------------------------------------------------------------------------
+;; cdlatex
+;;----------------------------------------------------------------------------
+(add-hook 'LaTeX-mode-hook
+          (lambda ()
+            (cdlatex-mode t)
+            (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
+            (setq TeX-command-default "XeLaTeX")
+            (setq TeX-save-query  nil )
+            (setq TeX-show-compilation t)))
 
 (provide 'init-latex)
