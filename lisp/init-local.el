67c5a9e6c0e7b33b1a36386a2426620de3a2edfc
cdadar <gunbanmi@gmail.com>
2017-08-22 16:39:30 +0800

    add python setting

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,11 +1,18 @@
+;; org setting
 (setq org-default-notes-file "~/data/org-model/capture.org"
       org-directory "~/data/org-model/"
       exec-path-from-shell-arguments '("-i"))
 
-
+;; org mobile
 ;;(setq org-mobile-files (list "~/data/org-model/plan.org"))
 (setq org-mobile-inbox-for-pull "~/Dropbox/Apps/MobileOrg/index.org")
 (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+
+
+;; python
+(setq python-shell-interpreter "python3")
+
+
 
 
 (provide 'init-local)
