ea636a40812b0a8aa0558f01f689b153b4a8dd21
cdadar <gunbanmi@gmail.com>
2017-09-20 10:46:42 +0800

    hidden yas-minor-mode

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -1,15 +1,16 @@
 ;; loading yasnippet will slow the startup
 ;; but it's necessary cost
 
-(require-package 'yasnippet)
+(when (maybe-require-package 'yasnippet)
+  (yas-global-mode t)
+  ;;加载模板位置
+  (setq yas/root-directory (expand-file-name "./snippets" user-emacs-directory))
+  (yas/load-directory yas/root-directory)
 
+  (setf yas-indent-line 'fixed)
 
-
-(yas-global-mode t)
-;;加载模板位置
-(setq yas/root-directory (expand-file-name "./snippets" user-emacs-directory))
-(yas/load-directory yas/root-directory)
-
-(setf yas-indent-line 'fixed)
+  (when (maybe-require-package 'diminish)
+    (after-load 'yasnippet
+      (diminish 'yas-minor-mode))))
 
 (provide 'init-yasnippet)
