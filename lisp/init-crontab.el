21a607a896e489294a4eebb5c27182fbf8681c65
cdadar <gunbanmi@gmail.com>
2017-11-22 11:17:20 +0800

    add crontab

--- lisp/init-crontab.el
+++ lisp/init-crontab.el
@@ -1,4 +1,7 @@
-(require-package 'crontab-mode)
-(add-auto-mode 'crontab-mode "\\.?cron\\(tab\\)?\\'")
+(ensure-lib-from-url 'crontab-mode "http://www.mahalito.net/~harley/elisp/crontab-mode.el")
+
+(require 'crontab-mode)
+
+(add-auto-mode 'crontab-mode "\\.?cron\\(tab\\)?\\'" "cron\\(tab\\)?\\.*")
 
 (provide 'init-crontab)
