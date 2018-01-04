19f39f3be7e767e3601072292aa7c8515d4129d2
cdadar <gunbanmi@gmail.com>
2018-01-04 10:07:08 +0800

    modified cnfonts on window-setup

--- lisp/init-personal.el
+++ lisp/init-personal.el
@@ -41,6 +41,9 @@
 (when (maybe-require-package 'cnfonts)
   ;; 让 cnfonts 随着 Emacs 自动生效。
   (cnfonts-enable)
+
+  (add-hook 'after-make-frame-functions 'cnfonts-set-font-with-saved-step)
+  (add-hook 'window-setup-hook 'cnfonts-set-font-with-saved-step)
   ;; 让 spacemacs mode-line 中的 Unicode 图标正确显示。
   (cnfonts-set-spacemacs-fallback-fonts))
 
