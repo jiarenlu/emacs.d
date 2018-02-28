5b00294405e43865b58523c0f52fe03075d12b78
cdadar <gunbanmi@gmail.com>
2018-02-28 10:52:03 +0800

    recovrt init-exec-path

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -4,6 +4,7 @@
   (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
     (add-to-list 'exec-path-from-shell-variables var)))
 
-(exec-path-from-shell-initialize)
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
 
 (provide 'init-exec-path)
