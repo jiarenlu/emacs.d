cf22cfd9847bf47f79f3263a1458dfe7d0ddd8e3
cdadar <gunbanmi@gmail.com>
2018-03-08 10:15:35 +0800

    modified define-key of counsel-gtags-find-symbol

--- lisp/init-gtags.el
+++ lisp/init-gtags.el
@@ -4,7 +4,7 @@
   (after-load 'counsel-gtags
     (define-key counsel-gtags-mode-map (kbd "M-t") 'counsel-gtags-find-definition)
     (define-key counsel-gtags-mode-map (kbd "M-r") 'counsel-gtags-find-reference)
-    (define-key counsel-gtags-mode-map (kbd "M-s") 'counsel-gtags-find-symbol)
+    (define-key counsel-gtags-mode-map (kbd "M-s ,") 'counsel-gtags-find-symbol)
     (define-key counsel-gtags-mode-map (kbd "M-,") 'counsel-gtags-go-backward)))
 
 (provide 'init-gtags)
