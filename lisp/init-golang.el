adb7b33a1159a0834309c6d71f9b27739a3e396e
cdadar <gunbanmi@gmail.com>
2018-03-14 19:31:48 +0800

    modified init-golang

--- lisp/init-golang.el
+++ lisp/init-golang.el
@@ -1,4 +1,10 @@
 (when (maybe-require-package 'go-mode)
-  (maybe-require-package 'company-go))
+  (after-load 'go-mode
+    (add-hook 'go-mode-hook
+              (lambda () (setq-default tab-width 2))))
+  (when (maybe-require-package 'company-go)
+    (after-load 'company
+      (add-hook 'go-mode-hook
+                (lambda () (sanityinc/local-push-company-backend 'company-go))))))
 
 (provide 'init-golang)
