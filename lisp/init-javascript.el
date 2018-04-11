60d471e51cc3b1d19224a2edce4fe9dad448e111
cdadar <gunbanmi@gmail.com>
2018-04-11 15:51:27 +0800

    add js2-refactor

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -110,5 +110,9 @@
   (after-load 'js2-mode
     (add-hook 'js2-mode-hook 'add-node-modules-path)))
 
+(when (maybe-require-package 'js2-refactor)
+  (add-hook 'js2-mode-hook #'js2-refactor-mode)
+  (js2r-add-keybindings-with-prefix "C-c C-m"))
+
 
 (provide 'init-javascript)
