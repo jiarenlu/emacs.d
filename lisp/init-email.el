62bda6928ca096a1fe5c919c87b098aaa63ca6ae
cdadar <gunbanmi@gmail.com>
2018-04-23 18:16:56 +0800

    error because not install mu4e

--- lisp/init-email.el
+++ lisp/init-email.el
@@ -6,22 +6,24 @@
 (add-to-list 'load-path mu4e-installation-path)
 
 
-(require 'mu4e)
+
+(when (file-exists-p mu4e-installation-path)
+  (require 'mu4e)
 
 
-(setq mu4e-maildir       "~/mail"
-      mu4e-sent-folder   "/sent"
-      mu4e-drafts-folder "/drafts"
-      mu4e-trash-folder  "/trash"
-      mu4e-refile-folder "/archive")
+  (setq mu4e-maildir       "~/mail"
+        mu4e-sent-folder   "/sent"
+        mu4e-drafts-folder "/drafts"
+        mu4e-trash-folder  "/trash"
+        mu4e-refile-folder "/archive")
 
-(setq mu4e-get-mail-command "getmail"
-      mu4e-update-interval 300)
+  (setq mu4e-get-mail-command "getmail"
+        mu4e-update-interval 300)
 
-(setq message-send-mail-function 'smtpmail-send-it
-      smtpmail-smtp-server "smtp.exmail.qq.com"
-      smtpmail-smtp-service 465)
-(setq user-full-name "chens")
-(setq user-mail-address "chens@fangde.com")
+  (setq message-send-mail-function 'smtpmail-send-it
+        smtpmail-smtp-server "smtp.exmail.qq.com"
+        smtpmail-smtp-service 465)
+  (setq user-full-name "chens")
+  (setq user-mail-address "chens@fangde.com"))
 
 (provide 'init-email)
