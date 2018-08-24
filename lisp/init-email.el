4cfcf53aead0e3832e8bd98c0eb9f926f44a5b19
cdadar <gunbanmi@gmail.com>
2018-08-24 09:54:52 +0800

    add mu4e-alert

--- lisp/init-email.el
+++ lisp/init-email.el
@@ -24,6 +24,20 @@
         smtpmail-smtp-server "smtp.exmail.qq.com"
         smtpmail-smtp-service 465)
   (setq user-full-name "chens")
-  (setq user-mail-address "chens@fangde.com"))
+  (setq user-mail-address "chens@fangde.com")
+
+
+  (when (maybe-require-package 'mu4e-alert)
+    ;; Choose the style you prefer for desktop notifications
+    ;; If you are on Linux you can use
+    ;; 1. notifications - Emacs lisp implementation of the Desktop Notifications API
+    ;; 2. libnotify     - Notifications using the `notify-send' program, requires `notify-send' to be in PATH
+    ;;
+    ;; On Mac OSX you can set style to
+    ;; 1. notifier      - Notifications using the `terminal-notifier' program, requires `terminal-notifier' to be in PATH
+    ;; 1. growl         - Notifications using the `growl' program, requires `growlnotify' to be in PATH
+    (mu4e-alert-set-default-style 'libnotify)
+    (add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
+    (add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display)))
 
 (provide 'init-email)
