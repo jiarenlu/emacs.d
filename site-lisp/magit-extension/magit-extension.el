1bd0c70308f9c8e9c741ca06a3c1ee63be14b44d
cdadar <gunbanmi@gmail.com>
2018-11-01 14:29:02 +0800

    udpate magit-extension

--- site-lisp/magit-extension/magit-extension.el
+++ site-lisp/magit-extension/magit-extension.el
@@ -6,8 +6,8 @@
 ;; Maintainer: Andy Stewart <lazycat.manatee@gmail.com>
 ;; Copyright (C) 2018, Andy Stewart, all rights reserved.
 ;; Created: 2018-09-07 03:32:14
-;; Version: 0.1
-;; Last-Updated: 2018-09-07 03:32:14
+;; Version: 0.2
+;; Last-Updated: 2018-09-10 21:24:05
 ;;           By: Andy Stewart
 ;; URL: http://www.emacswiki.org/emacs/download/magit-extension.el
 ;; Keywords:
@@ -65,6 +65,9 @@
 
 ;;; Change log:
 ;;
+;; 2018/09/10
+;;      * Fix error of `magit-submodule-remove' that git-modules-submodule-path is not right sometimes.
+;;
 ;; 2018/09/07
 ;;      * First released.
 ;;
@@ -84,70 +87,49 @@
 
 ;;; Code:
 
+(defun magit-get-submodule-short-name (path)
+  "Return short name of submodule path."
+  (let* ((submodule-lines (magit-git-lines "config" "--list" "-f" ".gitmodules"))
+         (submodule-match-line (car (seq-filter (lambda (l) (string-match (format "submodule.*.path=%s" path) l)) submodule-lines))))
+    (when submodule-match-line
+      (string-remove-suffix ".path" (string-remove-prefix "submodule." (car (split-string submodule-match-line "=")))))))
+
+;;;###autoload
 (defun magit-submodule-remove (&optional module-name)
   (interactive)
-  (save-excursion
-    ;; Get current directory for restore after remove submodule.
-    (setq current-directory default-directory)
-    ;; Cd magit toplevel directory make sure `magit-list-module-paths' can work.
-    (cd (magit-toplevel))
-    ;; Get subodule name/path will to remove.
-    (setq submodule-name (or module-name (completing-read "Remove submodule: " (magit-list-module-paths))))
-    (setq submodule-path (concat (magit-toplevel) submodule-name))
-    ;; Delete submodule directory first.
-    (when (file-exists-p submodule-path)
-      (delete-directory submodule-path t))
-    ;; Delete submodule from .gitmodules file.
-    (setq gitmodules-file-path (concat (magit-toplevel) ".gitmodules"))
-    (magit-kill-buffer-match-file gitmodules-file-path)
-    (with-current-buffer (find-file gitmodules-file-path)
-      (goto-char (point-min))
-      (when (search-forward-regexp (format "path\\s-=\\s-%s" submodule-name) nil t)
-        (previous-line)
-        (beginning-of-line)
-        (kill-line 3)
-        (save-buffer)))
-    ;; Get submodule url.
-    (setq git-modules-submodule-path (concat (magit-toplevel) ".git/modules/" (file-name-base submodule-name)))
-    (magit-kill-buffer-match-file (concat git-modules-submodule-path "/config"))
-    (with-current-buffer (find-file (concat git-modules-submodule-path "/config"))
-      (goto-char (point-min))
-      (when (search-forward-regexp "^\\[remote\\s-\"origin\"\\]" nil t)
-        (when (search-forward-regexp "url\\s-=\\s-" nil t)
-          (setq submodule-url (buffer-substring-no-properties
-                               (point)
-                               (save-excursion
-                                 (end-of-line)
-                                 (point))))
-          )))
-    ;; Delete submodule from .git/config file.
-    (setq git-config-file-path (concat (magit-toplevel) ".git/" "config"))
-    (magit-kill-buffer-match-file git-config-file-path)
-    (with-current-buffer (find-file git-config-file-path)
-      (goto-char (point-min))
-      (when (search-forward-regexp (format "url\\s-=\\s-%s" submodule-url) nil t)
-        (previous-line)
-        (beginning-of-line)
-        (kill-line 3)
-        (save-buffer)))
-    ;; Delete submodule under .git/modules/ directory.
-    (when (file-exists-p git-modules-submodule-path)
-      (delete-directory git-modules-submodule-path t))
-    ;; Kill temp buffer.
-    (magit-kill-buffer-match-file gitmodules-file-path)
-    (magit-kill-buffer-match-file (concat git-modules-submodule-path "/config"))
-    (magit-kill-buffer-match-file git-config-file-path)
-    ;; Restore current directory.
-    (cd current-directory)
-    ))
+  (let* ((default-directory (caar (magit-list-worktrees)))
+         (submodule-name (or module-name (completing-read "Remove submodule: " (magit-list-module-paths))))
+         (submodule-short-name (magit-get-submodule-short-name submodule-name))
+         (submodule-fullpath (concat default-directory submodule-name))
+         (submodule-modules-path (concat default-directory
+                                         (file-name-as-directory ".git")
+                                         (file-name-as-directory "modules")
+                                         (magit-get-submodule-name submodule-name))))
+    ;; Remove the submodule entry from .git/config
+    (magit-run-git "submodule" "deinit" "-f" submodule-name)
 
-(defun magit-kill-buffer-match-file (filepath)
-  (catch 'find-match
-    (dolist (buffer (buffer-list))
-      (when (string-equal (buffer-file-name buffer) filepath)
-        (kill-buffer buffer)
-        (throw 'find-match buffer)))
-    nil))
+    ;; Delete the submodule entry from .gitmodules file.
+    (magit-run-git "config" "-f" ".gitmodules" "--remove-section" (format "submodule.%s" submodule-short-name))
+
+    ;; Delete submodule directory.
+    (when (file-exists-p submodule-fullpath)
+      (delete-directory submodule-fullpath t))
+
+    ;; Delete submodule under .git/modules/ directory.
+    (when (file-exists-p submodule-modules-path)
+      (delete-directory submodule-modules-path t))))
+
+(defun magit-get-submodule-url (path)
+  "Return url of submodule path."
+  (let* ((submodule-lines (magit-git-lines "config" "--list" "-f" ".gitmodules"))
+         (submodule-match-line (car (seq-filter (lambda (l) (string-match (format "submodule.*.path=%s" path) l)) submodule-lines)))
+         submodule-url-title
+         submodule-url-string)
+    (when submodule-match-line
+      (let* ((submodule-url-title (replace-regexp-in-string ".path$" ".url" (car (split-string submodule-match-line "="))))
+             (submodule-url-string (car (seq-filter (lambda (l) (string-match (format "%s=*" submodule-url-title) l)) submodule-lines))))
+        (when submodule-url-string
+          (replace-regexp-in-string "submodule.*url=" "" submodule-url-string))))))
 
 (provide 'magit-extension)
 
