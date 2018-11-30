20e6b7a1bd9ff88d008a22d0f75dd34458e9d206
re-mx <reimu-mx@outlook.com>
2018-11-30 19:01:00 +0800

    Move flyspell config to init-spelling.el

--- lisp/init-spelling.el
+++ lisp/init-spelling.el
@@ -189,4 +189,12 @@ Please note RUN-TOGETHER will make aspell less capable. So it should only be use
                       (length aspell-words)
                       (mapconcat 'identity aspell-words "\n"))))))
 
+(when (executable-find ispell-program-name)
+  ;; Add spell-checking in comments for all programming language modes
+  (add-hook 'prog-mode-hook 'flyspell-prog-mode)
+
+  (after-load 'flyspell
+    (define-key flyspell-mode-map (kbd "C-;") nil)
+    (add-to-list 'flyspell-prog-text-faces 'nxml-text-face)))
+
 (provide 'init-spelling)
