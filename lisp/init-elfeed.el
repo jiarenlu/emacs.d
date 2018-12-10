0d6679a0fccbdfacb3f13aa2b1f2148c0de97306
cdadar <gunbanmi@gmail.com>
2018-12-10 09:13:13 +0800

    modified elfeed

--- lisp/init-elfeed.el
+++ lisp/init-elfeed.el
@@ -1,24 +1,25 @@
-(after-load 'org
-  (when (maybe-require-package 'elfeed)
-    (global-set-key (kbd "C-x w") 'elfeed)
-    (after-load 'elfeed
-      (define-key elfeed-search-mode-map "q" 'bjm/elfeed-save-db-and-bury))
+(when (maybe-require-package 'elfeed)
+  (global-set-key (kbd "C-x w") 'elfeed)
+  (after-load 'elfeed
+    (define-key elfeed-search-mode-map "q" 'bjm/elfeed-save-db-and-bury))
 
-    ;;functions to support syncing .elfeed between machines
-    ;;makes sure elfeed reads index from disk before launching
-    (defun bjm/elfeed-load-db-and-open ()
-      "Wrapper to load the elfeed db from disk before opening"
-      (interactive)
-      (elfeed-db-load)
-      (elfeed)
-      (elfeed-search-update--force))
+  ;;functions to support syncing .elfeed between machines
+  ;;makes sure elfeed reads index from disk before launching
+  (defun bjm/elfeed-load-db-and-open ()
+    "Wrapper to load the elfeed db from disk before opening"
+    (interactive)
+    (elfeed-db-load)
+    (elfeed)
+    (elfeed-search-update--force))
 
-    ;;write to disk when quiting
-    (defun bjm/elfeed-save-db-and-bury ()
-      "Wrapper to save the elfeed db to disk before burying buffer"
-      (interactive)
-      (elfeed-db-save)
-      (quit-window))
+  ;;write to disk when quiting
+  (defun bjm/elfeed-save-db-and-bury ()
+    "Wrapper to save the elfeed db to disk before burying buffer"
+    (interactive)
+    (elfeed-db-save)
+    (quit-window))
+
+  (after-load 'org
     (when (maybe-require-package 'elfeed-org)
       (elfeed-org))))
 
