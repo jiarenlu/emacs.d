9fc7339dde6dfebe603d786b9cd77feb47b5d5b6
cdadar <gunbanmi@gmail.com>
2019-01-16 14:11:46 +0800

    add anki-editor

--- init.el
+++ init.el
@@ -169,6 +169,7 @@
 (require 'init-rpm)
 ;; Extra packages which don't require any configuration
 
+(maybe-require-package 'anki-editor)
 (require-package 'gnuplot)
 (require-package 'lua-mode)
 (require-package 'htmlize)
