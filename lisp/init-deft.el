f7ba989d6b3a6654ff710f6d87fd237b50efd7e4
cdadar <gunbanmi@gmail.com>
2019-05-10 16:37:13 +0800

    update deft settings

--- lisp/init-deft.el
+++ lisp/init-deft.el
@@ -6,7 +6,9 @@
 
 (when (maybe-require-package 'deft)
   (setq deft-directory "~/org-mode"
-        deft-extensions '("md" "org"))
+        deft-extensions '("md" "org")
+        deft-text-mode 'org-mode
+        deft-use-filename-as-title t)
   (global-set-key (kbd "<f8>") 'deft)
   (global-set-key (kbd "C-x C-g") 'deft-find-file))
 
