5b8f7863c61b95f8ddcdc297017bf39cb28f1a6d
cdadar <gunbanmi@gmail.com>
2019-05-13 10:25:23 +0800

    update deft setting

--- lisp/init-deft.el
+++ lisp/init-deft.el
@@ -6,10 +6,13 @@
 
 (when (maybe-require-package 'deft)
   (setq deft-extensions '("md" "org")
+        deft-default-extension "org"
         deft-text-mode 'org-mode
         deft-use-filename-as-title t
         deft-use-filter-string-for-filename t
-        deft-recursive t)
+        deft-recursive t
+        deft-file-naming-rules '((nospace . "-"))
+        deft-org-mode-title-prefix t)
 
   (global-set-key (kbd "<f8>") 'deft)
   (global-set-key (kbd "C-x C-g") 'deft-find-file))
