c09cf61afdb9a4d3005e942b7e7ad03e9b9243b9
cdadar <gunbanmi@gmail.com>
2019-05-22 10:25:43 +0800

    update golang config

--- lisp/init-golang.el
+++ lisp/init-golang.el
@@ -3,13 +3,31 @@
 ;;; Code:
 
 ;; -*- lexical-binding: t -*-
+
+;;; Basic golang setup
 (when (maybe-require-package 'go-mode)
   (after-load 'go-mode
+
+    (add-auto-mode 'go-mode "\\.go\\'")
+
+    (add-hook 'go-mode-hook (lambda ()
+                              (setq tab-width 4)
+                              (exec-path-from-shell-copy-env "GOPATH")))
+    (add-hook 'go-mode-hook 'subword-mode)
+
     (add-hook 'go-mode-hook
               (lambda () (setq-default tab-width 2))))
+
+
+  ;;; Company-go
   (when (maybe-require-package 'company-go)
     (after-load 'company
       (add-hook 'go-mode-hook
-                (push 'company-go company-backends)))))
+                (push 'company-go company-backends))))
+
+  ;;; Golang eldoc
+  (when (maybe-require-package 'go-eldoc)
+    (after-load 'go-eldoc
+      (add-hook 'go-mode-hook 'go-eldoc-setup))))
 
 (provide 'init-golang)
