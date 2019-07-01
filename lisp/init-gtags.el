9d03a47dd7e8afa4e2c9e63b9fad2c3195ca22f1
cdadar <gunbanmi@gmail.com>
2019-07-01 18:01:45 +0800

    update gtags

--- lisp/init-gtags.el
+++ lisp/init-gtags.el
@@ -10,10 +10,10 @@
   (add-hook 'c++-mode-hook 'counsel-gtags-mode)
   (add-hook 'php-mode-hook 'counsel-gtags-mode)
   (after-load 'counsel-gtags
-    (define-key counsel-gtags-mode-map (kbd "M-t") 'counsel-gtags-find-definition)
-    (define-key counsel-gtags-mode-map (kbd "M-r") 'counsel-gtags-find-reference)
-    (define-key counsel-gtags-mode-map (kbd "M-s ,") 'counsel-gtags-find-symbol)
-    (define-key counsel-gtags-mode-map (kbd "M-,") 'counsel-gtags-go-backward)
+    ;; (define-key counsel-gtags-mode-map (kbd "M-t") 'counsel-gtags-find-definition)
+    ;; (define-key counsel-gtags-mode-map (kbd "M-r") 'counsel-gtags-find-reference)
+    ;; (define-key counsel-gtags-mode-map (kbd "M-s ,") 'counsel-gtags-find-symbol)
+    ;; (define-key counsel-gtags-mode-map (kbd "M-,") 'counsel-gtags-go-backward)
     ;; (add-hook 'after-save-hook 'counsel-gtags-update-tags)
     ))
 
@@ -25,7 +25,20 @@
     '(progn
        (define-key ggtags-navigation-map (kbd "M-<") nil)
        (define-key ggtags-navigation-map (kbd "M->") nil)
-       (add-hook 'xref-backend-functions #'ggtags--xref-backend nil t))))
+       (define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
+       (define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
+       (define-key ggtags-mode-map (kbd "C-c g r") 'ggtags-find-reference)
+       (define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
+       (define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
+       (define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
+       (define-key ggtags-mode-map (kbd "C-c g a") 'counsel-gtags-find-definition)
+       (define-key ggtags-mode-map (kbd "M-.") 'ggtags-find-tag-dwim)
+       (define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)
+       (define-key ggtags-mode-map (kbd "C-c <") 'ggtags-prev-mark)
+       (define-key ggtags-mode-map (kbd "C-c >") 'ggtags-next-mark)))
+
+(when (maybe-require-package 'gxref)
+  (add-to-list 'xref-backend-functions 'gxref-xref-backend))
 
 
 
