838b9edac963dbb17df1c973444b885d7461769f
cdadar <gunbanmi@gmail.com>
2019-07-03 12:39:31 +0800

    update helm config update

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -1,173 +1,76 @@
-;;; init-helm.el --- Init helm
-
-;; Filename: init-helm.el
-;; Description: Init helm
-;; Author: Andy Stewart <andy@freedom>
-;; Maintainer: Andy Stewart <andy@freedom>
-;; Copyright (C) 2013, Andy Stewart, all rights reserved.
-;; Created: 2013-12-30 16:23:29
-;; Version: 1.0
-;; Last-Updated: 2018-11-01 20:53:46
-;;           By: Andy Stewart
-;; URL: http://www.emacswiki.org/emacs/download/init-helm.el
-;; Keywords:
-;; Compatibility: GNU Emacs 24.3.50.1
-;;
-;; Features that might be required by this library:
-;;
-;; `helm' `helm-buffers'
-;; `helm-c-yasnippet' `helm-for-files'
-;; `helm-ring'
-;;
-
-;;; This file is NOT part of GNU Emacs
-
-;;; License
-;;
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program; see the file COPYING.  If not, write to
-;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
-;; Floor, Boston, MA 02110-1301, USA.
-
+;;; init-ivy.el --- Use ivy for completion and more -*- lexical-binding: t -*-
 ;;; Commentary:
-;;
-;; Init helm
-;;
-
-;;; Installation:
-;;
-;; Put init-helm.el to your load-path.
-;; The load-path is usually ~/elisp/.
-;; It's set in your ~/.emacs like this:
-;; (add-to-list 'load-path (expand-file-name "~/elisp"))
-;;
-;; And the following to your ~/.emacs startup file.
-;;
-;; (require 'init-helm)
-;;
-;; In Mac OS, you perhaps need execute below command to build locate db:
-;;
-;; sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist
-;;
-
-;;; Customize:
-;;
-;;
-;;
-;; All of the above can customize by:
-;;      M-x customize-group RET init-helm RET
-;;
-
-;;; Change log:
-;;
-;; 2018/11-01
-;;      * Fix `helm-ls-git' not work when first run `helm-dwim'.
-;;
-;; 2018/10-29
-;;      * Use `helm-ls-git' instead `helm-projectile', helm so fast now!!!
-;;
-;; 2018/09/03
-;;      * Make filename has enough width to display full name.
-;;
-;; 2018/07/26
-;;      * Make `helm-source-buffers-list' first, otherwise can't switch to with exist buffer list.
-;;
-;; 2018/07/12
-;;      * Add `helm-source-locate' in `helm-source-list' for search file in file system.
-;;
-;; 2018/06/29
-;;      * Remove `helm-autoload-commands', one-key is enough.
-;;
-;; 2018/06/28
-;;      * Update some key of helm-map.
-;;
-;; 2018/06/14
-;;      * Upgrade configuration to match git version of helm.
-;;      * Just add helm-source-projectile-* in list when current place in project, avoid `helm-source-projectile-files-list' block `helm-dwim'.
-;;
-;; 2013/12/30
-;;      * First released.
-;;
-
-;;; Acknowledgements:
-;;
-;;
-;;
-
-;;; TODO
-;;
-;;
-;;
+;;; Code:
 
 
-;; (lazy-load-set-keys
-;;  '(
-;;    ("M-s-j" . helm-next-source)
-;;    ("M-s-k" . helm-previous-source)
-;;    ("M->" . helm-scroll-other-window-down)
-;;    ("M-<" . helm-scroll-other-window)
-;;    ("M-o" . backward-delete-char-untabify)
-;;    ("M-m" . move-beginning-of-line)
-;;    )
-;;  helm-map)
 
 
-(after-load 'helm
+(when (maybe-require-package 'helm)
+  (helm-mode 1)
+  (after-load 'helm
 
-  (require 'helm-config)
+    (require 'helm-config)
 
-  (setq helm-buffer-max-length 60) ; make filename has enough width to display full name
+    (setq helm-buffer-max-length 60) ; make filename has enough width to display full name
 
-  (defvar helm-source-elisp-library
-    (helm-build-in-buffer-source  "Elisp libraries (Scan)"
-      :data #'helm-locate-library-scan-list
-      :fuzzy-match helm-locate-library-fuzzy-match
-      :keymap helm-generic-files-map
-      :search (unless helm-locate-library-fuzzy-match
-                (lambda (regexp)
-                  (re-search-forward
-                   (if helm-ff-transformer-show-only-basename
-                       (replace-regexp-in-string
-                        "\\`\\^" "" regexp)
-                     regexp)
-                   nil t)))
-      :match-part (lambda (candidate)
-                    (if helm-ff-transformer-show-only-basename
-                        (helm-basename candidate) candidate))
-      :filter-one-by-one (lambda (c)
-                           (if helm-ff-transformer-show-only-basename
-                               (cons (helm-basename c) c) c))
-      :action (helm-actions-from-type-file)))
+    (when (executable-find "curl")
+      (setq helm-google-suggest-use-curl-p t))
 
-  (define-key helm-map (kbd "M-s-j") 'helm-next-source)
-  (define-key helm-map (kbd "M-s-k") 'helm-previous-source)
-  (define-key helm-map (kbd "M->") 'helm-scroll-other-window-down)
-  (define-key helm-map (kbd "M-<") 'helm-scroll-other-window)
-  (define-key helm-map (kbd "M-o") 'backward-delete-char-untabify)
-  (define-key helm-map (kbd "M-m") 'move-beginning-of-line)
+    (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+          helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+          helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+          helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+          helm-ff-file-name-history-use-recentf t
+          helm-echo-input-in-header-line t)
 
 
-  (global-set-key (kbd "C-x C-f") #'helm-find-files)
-  (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
-  (global-set-key (kbd "C-c h") #'helm-mini)
+    (setq helm-autoresize-max-height 0)
+    (setq helm-autoresize-min-height 20)
+    (helm-autoresize-mode 1)
 
-  (when (maybe-require-package 'projectile)
-    (when (maybe-require-package 'helm-projectile)
-      (helm-projectile-on)))
+    (defvar helm-source-elisp-library
+      (helm-build-in-buffer-source  "Elisp libraries (Scan)"
+        :data #'helm-locate-library-scan-list
+        :fuzzy-match helm-locate-library-fuzzy-match
+        :keymap helm-generic-files-map
+        :search (unless helm-locate-library-fuzzy-match
+                  (lambda (regexp)
+                    (re-search-forward
+                     (if helm-ff-transformer-show-only-basename
+                         (replace-regexp-in-string
+                          "\\`\\^" "" regexp)
+                       regexp)
+                     nil t)))
+        :match-part (lambda (candidate)
+                      (if helm-ff-transformer-show-only-basename
+                          (helm-basename candidate) candidate))
+        :filter-one-by-one (lambda (c)
+                             (if helm-ff-transformer-show-only-basename
+                                 (cons (helm-basename c) c) c))
+        :action (helm-actions-from-type-file)))
 
-  (when (maybe-require-package 'helm-smex)
-    (global-set-key [remap execute-extended-command] #'helm-smex)
-    (global-set-key (kbd "M-X") #'helm-smex-major-mode-commands)
-    ))
+
+
+    (define-key helm-map (kbd "M-s-j") 'helm-next-source)
+    (define-key helm-map (kbd "M-s-k") 'helm-previous-source)
+    (define-key helm-map (kbd "M->") 'helm-scroll-other-window-down)
+    (define-key helm-map (kbd "M-<") 'helm-scroll-other-window)
+    (define-key helm-map (kbd "M-o") 'backward-delete-char-untabify)
+    (define-key helm-map (kbd "M-m") 'move-beginning-of-line)
+
+
+    (global-set-key (kbd "C-x C-f") #'helm-find-files)
+    (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+    (global-set-key (kbd "C-c h") #'helm-mini)
+
+    (when (maybe-require-package 'projectile)
+      (when (maybe-require-package 'helm-projectile)
+        (helm-projectile-on)))
+
+    (when (maybe-require-package 'helm-smex)
+      (global-set-key [remap execute-extended-command] #'helm-smex)
+      (global-set-key (kbd "M-X") #'helm-smex-major-mode-commands)
+      )))
 
 (provide 'init-helm)
+;;; init-helm.el ends here
