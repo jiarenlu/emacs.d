aa3aaeed72cea6cf36b9653f500da6ec9a370da7
cdadar <gunbanmi@gmail.com>
2019-07-17 09:33:03 +0800

    update counsel-gtags keybind

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -89,9 +89,9 @@ instead."
   (add-hook 'c++-mode-hook 'counsel-gtags-mode)
   (add-hook 'php-mode-hook 'counsel-gtags-mode)
   (after-load 'counsel-gtags
-    (define-key counsel-gtags-mode-map (kbd "M-s t") 'counsel-gtags-find-definition)
-    (define-key counsel-gtags-mode-map (kbd "M-s r") 'counsel-gtags-find-reference)
-    (define-key counsel-gtags-mode-map (kbd "M-s ,") 'counsel-gtags-find-symbol)
+    ;; (define-key counsel-gtags-mode-map (kbd "M-s t") 'counsel-gtags-find-definition)
+    ;; (define-key counsel-gtags-mode-map (kbd "M-s r") 'counsel-gtags-find-reference)
+    ;; (define-key counsel-gtags-mode-map (kbd "M-s ,") 'counsel-gtags-find-symbol)
     (add-hook 'after-save-hook 'counsel-gtags-update-tags)
     ))
 (when (maybe-require-package 'ivy-xref)
