6db4fea215bdf8dfce6e290dcf401c0696164e59
cdadar <gunbanmi@gmail.com>
2019-07-29 15:48:11 +0800

    drop global-emojify-mode

--- lisp/init-emojify.el
+++ lisp/init-emojify.el
@@ -4,7 +4,6 @@
 
 ;; -*- lexical-binding: t -*-
 (maybe-require-package 'emojify)
-(add-hook 'after-init-hook #'global-emojify-mode)
 (after-load 'emojify-mode
   (when (maybe-require-package 'company-emoji)
     (add-to-list 'company-backends 'company-emoji)))
