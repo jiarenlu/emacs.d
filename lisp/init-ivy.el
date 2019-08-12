ed9a9544d32cd1aa8707295bb9f969fd70630b08
cdadar <gunbanmi@gmail.com>
2019-08-12 14:05:25 +0800

    update counsel-etags update-tags and config

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -80,6 +80,15 @@ instead."
 
 
 (when (maybe-require-package 'counsel-etags)
+  ;; Don't ask before rereading the TAGS files if they have changed
+  (setq tags-revert-without-query t)
+  ;; Don't warn when TAGS files are large
+  (setq large-file-warning-threshold nil)
+  ;; Setup auto update now
+  (add-hook 'prog-mode-hook
+            (lambda ()
+              (add-hook 'after-save-hook
+                        'counsel-etags-virtual-update-tags 'append 'local)))
   (after-load 'ivy
     (define-key ivy-mode-map (kbd "C-c e .") 'counsel-etags-find-tag-at-point)
     (define-key ivy-mode-map (kbd "C-c e t") 'counsel-etags-recent-tag)
