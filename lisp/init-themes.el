2cfdfc8c9daf8e79372d3390bb343dcf086d6ff9
cdadar <gunbanmi@gmail.com>
2019-08-24 00:32:21 +0800

    add solarized-theme

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -4,9 +4,10 @@
 
 (require-package 'color-theme-sanityinc-solarized)
 (require-package 'color-theme-sanityinc-tomorrow)
+(require-package 'solarized-theme)
 
 ;; If you don't customize it, this is the theme you get.
-(setq-default custom-enabled-themes '(sanityinc-tomorrow-eighties))
+(setq-default custom-enabled-themes '(sanityinc-tomorrow-night))
 
 ;; Ensure that themes will be applied even if they have not been customized
 (defun reapply-themes ()
