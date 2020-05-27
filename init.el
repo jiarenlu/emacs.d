2cc0a9ef2808028e0045fe77c10f9851ca28bfca
Steve Purcell <steve@sanityinc.com>
2020-05-28 08:59:04 +1200

    Add direnv support using envrc.el

--- init.el
+++ init.el
@@ -191,6 +191,8 @@
 (when (fboundp 'global-eldoc-mode)
   (add-hook 'after-init-hook 'global-eldoc-mode))
 
+(require 'init-direnv)
+
 ;;----------------------------------------------------------------------------
 ;; Allow access from emacsclient
 ;;----------------------------------------------------------------------------
