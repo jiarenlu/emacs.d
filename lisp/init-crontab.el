bc34543ad2483ac22b7307e15b4a57ee2d24b7d7
cdadar <gunbanmi@gmail.com>
2021-02-27 21:15:56 +0800

    replace crontab-mode from mepla to from url

--- lisp/init-crontab.el
+++ lisp/init-crontab.el
@@ -1,15 +1,8 @@
 ;;; init-crontab.el --- Working with crontabs -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
-
-(ensure-lib-from-url 'crontab-mode "http://www.mahalito.net/~harley/elisp/crontab-mode.el")
-
-(require 'crontab-mode)
-
+(require-package 'crontab-mode)
 (add-auto-mode 'crontab-mode "\\.?cron\\(tab\\)?\\'" "\\^cron\\(tab\\)?\\.*")
-
-;; (require-package 'crontab-mode)
-;; (add-auto-mode 'crontab-mode "\\.?cron\\(tab\\)?\\'")
 
 (provide 'init-crontab)
 ;;; init-crontab.el ends here
