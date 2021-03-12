4c7ae9e8bd4ac5b7faa4616b8a327b4f0db8d6f5
cdadar <gunbanmi@gmail.com>
2021-03-12 15:56:59 +0800

    update editing utils

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -376,7 +376,104 @@ ORIG is the advised function, which is called with its ARGS."
 
 (maybe-require-package 'iedit)
 
+;; 删除多余空白插件
+(when (maybe-require-package 'hungry-delete)
+  (require-package 'hungry-delete)
+  (global-hungry-delete-mode))
+
+
+
+;;同时编辑多个区域的插件
+;; (require-package 'iedit)
+;; (global-set-key (kbd "M-s e") 'iedit-mode)
+
+(when (maybe-require-package 'editorconfig)
+  (editorconfig-mode)
+  (with-eval-after-load 'projectile
+    '(progn
+       (add-hook 'projectile-after-switch-project-hook
+                 (lambda ()
+                   (let ((editorconfig-config-path (expand-file-name ".editorconfig" (projectile-project-root))))
+                     (when (file-exists-p editorconfig-config-path)
+                       (setq-local editorconfig-exec-path editorconfig-config-path))))))))
+
+;;禁止 Emacs 自动生成备份文件
+(setq make-backup-files nil)
+
+;; 将所选区域缩小到其先前的带大小的快捷键
+(with-eval-after-load 'expand-region
+  (global-set-key (kbd "C-|") 'er/contract-region))
+
+(when (maybe-require-package 'keyfreq)
+  (keyfreq-mode 1)
+  (keyfreq-autosave-mode 1))
+
+(when (maybe-require-package 'define-word)
+  (global-set-key (kbd "C-c C-d") 'define-word-at-point)
+  (global-set-key (kbd "C-c C-S-d") 'define-word))
+
+(when (maybe-require-package 'super-save)
+  (super-save-mode +1)
+  (setq auto-save-default nil)
+  (setq super-save-hook-triggers
+        (quote (focus-out-hook find-file-hook)))
+  (setq super-save-auto-save-when-idle t)
+  (setq super-save-idle-duration 300)
+  (setq super-save-remote-files nil)
+  (setq super-save-exclude '(".gpg"))
+  (setq super-save-triggers
+        '(other-window
+          balance-windows
+          next-buffer
+          org-babel-execute-src-block
+          previous-buffer
+          split-window-below
+          split-window-horizontally
+          start-process-shell-command
+          switch-to-buffer
+          windmove-down
+          windmove-left
+          windmove-right
+          windmove-up
+          magit-status))
+
+  (defun save-all-buffers ()
+    (save-excursion
+      (dolist (buf (buffer-list))
+        (set-buffer buf)
+        (when (and buffer-file-name
+                   (buffer-modified-p (current-buffer))
+                   (file-writable-p buffer-file-name)
+                   (if (file-remote-p buffer-file-name) super-save-remote-files t))
+          (save-buffer)))))
+
+  (advice-add 'super-save-command :override 'save-all-buffers))
+
+(when (maybe-require-package 'hl-todo)
+  (global-hl-todo-mode)
+  (with-eval-after-load 'hl-todo
+    '(progn
+       (define-key hl-todo-mode-map (kbd "C-c M-p") 'hl-todo-previous)
+       (define-key hl-todo-mode-map (kbd "C-c M-n") 'hl-todo-next)
+       (define-key hl-todo-mode-map (kbd "C-c M-o") 'hl-todo-occur))))
+
+(with-eval-after-load 'grep
+  '(progn
+     (define-key grep-mode-map
+       (kbd "C-x C-q") 'wgrep-change-to-wgrep-mode)
+     (define-key ag-mode-map
+       (kbd "C-x C-q") 'wgrep-change-to-wgrep-mode)))
+
+(with-eval-after-load 'wgrep
+  '(define-key grep-mode-map
+     (kbd "C-c C-c") 'wgrep-finish-edit))
+
+(require 'insert-translated-name)
+
+(require 'clip2org)
+
 (require 'advance-words-count)
+
 (require 'advance-wc-mode)
 
 (require 'delete-block)
